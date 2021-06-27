9982ea5dae1333aa6d936e7c9571349c5bf38101
Ashley Baldry <arbaldry91@gmail.com>
2021-06-27 18:39:07 +0100

    Changing button tag type

--- R/button.R
+++ R/button.R
@@ -3,7 +3,7 @@ howlerButton <- function(id, button_type = HOWLER_BUTTON_TYPES, ...) {
   button_type <- match.arg(button_type)
   button_id <- paste0(id, "_", button_type)
 
-  tags$button(id = button_id, class = paste0("howler-", button_type), ...)
+  tags$a(id = button_id, class = paste0("howler-", button_type), ...)
 }
 
 HOWLER_BUTTON_TYPES <- c("play_pause", "play", "next", "previous", "volumeup", "volumedown")
