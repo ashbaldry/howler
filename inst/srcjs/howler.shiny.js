8cb6cb6c948cfc0f69cf53dee6d0856331537e1d
Ashley Baldry <arbaldry91@gmail.com>
2021-06-29 19:35:39 +0100

    Reindent lines

--- inst/srcjs/howler.shiny.js
+++ inst/srcjs/howler.shiny.js
@@ -1,156 +1,156 @@
 var Howler = function(el) {
-    var self = this;
-    this.id = el.id;
-    this.index = 0;
+  var self = this;
+  this.id = el.id;
+  this.index = 0;
 
-    this.volume = el.dataset.volume;
-    this.seekRate = el.dataset.seekRate;
-    this.playlist = JSON.parse(el.dataset.audioFiles);
-    this.autoContinue = el.dataset.autocontinue === "TRUE";
-    this.autoLoop = el.dataset.autoloop === "TRUE";
-    this.autoPlayNext = true;
-    this.autoPlayPrevious = true;
+  this.volume = el.dataset.volume;
+  this.seekRate = el.dataset.seekRate;
+  this.playlist = JSON.parse(el.dataset.audioFiles);
+  this.autoContinue = el.dataset.autocontinue === "TRUE";
+  this.autoLoop = el.dataset.autoloop === "TRUE";
+  this.autoPlayNext = true;
+  this.autoPlayPrevious = true;
 
-    this.play = function() {
-        return new Howl({
-            src: self.playlist[self.index],
-            html5: true,
-            volume: self.volume,
+  this.play = function() {
+    return new Howl({
+      src: self.playlist[self.index],
+      html5: true,
+      volume: self.volume,
 
-            onload: function() {
-                Shiny.setInputValue(`${self.id}_track`, self.playlist[self.index]);
-                Shiny.setInputValue(`${self.id}_duration`, self.player.duration());
-                if (self.seekRate > 0) {
-                    Shiny.setInputValue(`${self.id}_seek`, self.player.seek());
-                }
-            },
-
-            onplay: function() {
-                Shiny.setInputValue(`${self.id}_playing`, true);
-
-                if (self.seekRate > 0) {
-                    setInterval(
-                        () => {
-                            var trackSeek = self.player.seek();
-                            Shiny.setInputValue(`${self.id}_seek`, Math.round(trackSeek * 100) / 100);
-                        },
-                        self.seekRate
-                    )
-                }
-            },
-
-            onpause: function() {
-                Shiny.setInputValue(`${self.id}_playing`, false);
-            },
-
-            onend: function() {
-                if (self.autoContinue && !(!self.autoLoop && self.index === (self.playlist.length - 1))) {
-                    self.changeNextTrack();
-                } else {
-                    Shiny.setInputValue(`${self.id}_playing`, false);
-                    self.setPlayPauseButton('play');
-                }
-            },
-
-            onseek: function() {
-                Shiny.setInputValue(`${self.id}_seek`, self.player.seek());
-            }
-        });
-    };
-
-    this.changeNextTrack = function(playTrack = true) {
-        if (self.index === (self.playlist.length - 1)) {
-            self.index = 0;
-        } else {
-            self.index++;
+      onload: function() {
+        Shiny.setInputValue(`${self.id}_track`, self.playlist[self.index]);
+        Shiny.setInputValue(`${self.id}_duration`, self.player.duration());
+        if (self.seekRate > 0) {
+          Shiny.setInputValue(`${self.id}_seek`, self.player.seek());
         }
+      },
 
-        self.changeTrack(playTrack);
-    };
+      onplay: function() {
+        Shiny.setInputValue(`${self.id}_playing`, true);
 
-    this.changePreviousTrack = function(playTrack = true) {
-        if (self.index === 0) {
-            self.index = self.playlist.length - 1;
-        } else {
-            self.index--;
+        if (self.seekRate > 0) {
+          setInterval(
+            () => {
+              var trackSeek = self.player.seek();
+              Shiny.setInputValue(`${self.id}_seek`, Math.round(trackSeek * 100) / 100);
+            },
+            self.seekRate
+          )
         }
+      },
 
-        self.changeTrack(playTrack);
+      onpause: function() {
+        Shiny.setInputValue(`${self.id}_playing`, false);
+      },
+
+      onend: function() {
+        if (self.autoContinue && !(!self.autoLoop && self.index === (self.playlist.length - 1))) {
+          self.changeNextTrack();
+        } else {
+          Shiny.setInputValue(`${self.id}_playing`, false);
+          self.setPlayPauseButton('play');
+        }
+      },
+
+      onseek: function() {
+        Shiny.setInputValue(`${self.id}_seek`, self.player.seek());
+      }
+    });
+  };
+
+  this.changeNextTrack = function(playTrack = true) {
+    if (self.index === (self.playlist.length - 1)) {
+      self.index = 0;
+    } else {
+      self.index++;
     }
 
-    this.changeTrack = function(playTrack = true) {
-        self.player.stop();
-        self.player = self.play();
+    self.changeTrack(playTrack);
+  };
 
-        if (playTrack) {
-            self.player.play();
-            self.setPlayPauseButton('pause');
-        }
-    };
+  this.changePreviousTrack = function(playTrack = true) {
+    if (self.index === 0) {
+      self.index = self.playlist.length - 1;
+    } else {
+      self.index--;
+    }
 
-    this.setPlayPauseButton = function(icon = 'play') {
-        var iconElement = document.getElementById(self.id + '_play_pause').firstElementChild;
+    self.changeTrack(playTrack);
+  }
 
-        if (iconElement) {
-            var addIcon = icon === 'play' ? 'fa-play' : 'fa-pause';
-            var removeIcon = icon === 'play' ? 'fa-pause' : 'fa-play';
-            $(iconElement).removeClass(removeIcon).addClass(addIcon);
-        }
-    };
+  this.changeTrack = function(playTrack = true) {
+    self.player.stop();
+    self.player = self.play();
 
-    this.player = this.play();
+    if (playTrack) {
+      self.player.play();
+      self.setPlayPauseButton('pause');
+    }
+  };
 
-    $(`#${this.id}_play`).on("click", function(e) {
-        self.player.play();
-    });
+  this.setPlayPauseButton = function(icon = 'play') {
+    var iconElement = document.getElementById(self.id + '_play_pause').firstElementChild;
 
-    $(`#${this.id}_play_pause`).on("click", function(e) {
-        if (self.player.playing()) {
-            self.player.pause();
-        } else {
-            self.player.play();
-        }
+    if (iconElement) {
+      var addIcon = icon === 'play' ? 'fa-play' : 'fa-pause';
+      var removeIcon = icon === 'play' ? 'fa-pause' : 'fa-play';
+      $(iconElement).removeClass(removeIcon).addClass(addIcon);
+    }
+  };
 
-        var icon = document.getElementById(this.id).firstElementChild;
-        $(icon).toggleClass('fa-play').toggleClass('fa-pause');
-    });
+  this.player = this.play();
 
-    $(`#${this.id}_pause`).on("click", function(e) {
-        self.player.pause();
-    });
+  $(`#${this.id}_play`).on("click", function(e) {
+    self.player.play();
+  });
 
-    $(`#${this.id}_stop`).on("click", function(e) {
-        self.player.stop();
-        Shiny.setInputValue(`${self.id}_playing`, false);
-    });
+  $(`#${this.id}_play_pause`).on("click", function(e) {
+    if (self.player.playing()) {
+      self.player.pause();
+    } else {
+      self.player.play();
+    }
 
-    $(`#${this.id}_next`).on("click", function(e) {
-        self.changeNextTrack(self.autoPlayNext);
-    });
+    var icon = document.getElementById(this.id).firstElementChild;
+    $(icon).toggleClass('fa-play').toggleClass('fa-pause');
+  });
 
-    $(`#${this.id}_previous`).on("click", function(e) {
-        self.changePreviousTrack(self.autoPlayPrevious);
-    });
+  $(`#${this.id}_pause`).on("click", function(e) {
+    self.player.pause();
+  });
 
-    $(`#${this.id}_volumeup`).on("click", function(e) {
-        var vol = Math.min(1, Number(self.player.volume()) + Number(this.dataset.volumeChange));
-        self.volume = vol;
-        self.player.volume(vol);
-    });
+  $(`#${this.id}_stop`).on("click", function(e) {
+    self.player.stop();
+    Shiny.setInputValue(`${self.id}_playing`, false);
+  });
 
-    $(`#${this.id}_volumedown`).on("click", function(e) {
-        var vol = Math.max(0, Number(self.player.volume()) - Number(this.dataset.volumeChange));
-        self.volume = vol;
-        self.player.volume(vol);
-    });
+  $(`#${this.id}_next`).on("click", function(e) {
+    self.changeNextTrack(self.autoPlayNext);
+  });
+
+  $(`#${this.id}_previous`).on("click", function(e) {
+    self.changePreviousTrack(self.autoPlayPrevious);
+  });
+
+  $(`#${this.id}_volumeup`).on("click", function(e) {
+    var vol = Math.min(1, Number(self.player.volume()) + Number(this.dataset.volumeChange));
+    self.volume = vol;
+    self.player.volume(vol);
+  });
+
+  $(`#${this.id}_volumedown`).on("click", function(e) {
+    var vol = Math.max(0, Number(self.player.volume()) - Number(this.dataset.volumeChange));
+    self.volume = vol;
+    self.player.volume(vol);
+  });
 }
 
 var howlerPlayers = [];
 
 $(document).on('shiny:connected', () => {
-    var howlers = document.getElementsByClassName('howler-player');
+  var howlers = document.getElementsByClassName('howler-player');
 
-    for (i = 0; i < howlers.length; i++) {
-        howlerPlayers.push(new Howler(howlers[i]));
-    }
+  for (i = 0; i < howlers.length; i++) {
+    howlerPlayers.push(new Howler(howlers[i]));
+  }
 });
