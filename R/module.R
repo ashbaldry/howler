a0e053f6364b4b97605e1e9db0ad3c76427e91f6
Ashley Baldry <arbaldry91@gmail.com>
2021-07-02 20:05:57 +0100

    Updating look of howler module

--- R/module.R
+++ R/module.R
@@ -1,7 +1,9 @@
 #' Howler.js Module
 #'
 #' @param id ID to give to the namespace of the module. The howler player will have the ID \code{{id}-howler}.
-#' @param files Vector of files
+#' @param files Files that will be used in the player. This can either be a single vector, or a list where different
+#' formats of the same file are kept in each element of the list.
+#' @param ... Other arguments sent to \code{\link{howlerPlayer}}
 #' @param input,output,session Standard \code{shiny} input, output and session
 #'
 #' @return
@@ -29,16 +31,19 @@
 #'
 #' @rdname howlerModule
 #' @export
-howlerModuleUI <- function(id, files) {
+howlerModuleUI <- function(id, files, ...) {
   ns <- NS(id)
   howler_id <- ns("howler")
 
   div(
     class = "howler-module",
-    howlerPlayer(howler_id, files),
-    previousButton(howler_id),
+    howlerPlayer(howler_id, ...),
+    if (length(files) > 1) previousButton(howler_id),
     playPauseButton(howler_id),
-    nextButton(howler_id),
+    if (length(files) > 1) nextButton(howler_id),
+    textOutput(ns("howler_seek"), inline = TRUE),
+    "/",
+    textOutput(ns("howler_duration"), inline = TRUE),
     volumeSlider(howler_id)
   )
 }
@@ -46,6 +51,14 @@ howlerModuleUI <- function(id, files) {
 #' @rdname howlerModule
 #' @export
 howlerModuleServer <- function(input, output, session) {
+  output$howler_seek <- renderText({
+    sprintf("%02d:%02.0f", input$howler_seek %/% 60, input$howler_seek %% 60)
+  })
+
+  output$howler_duration <- renderText({
+    sprintf("%02d:%02.0f", input$howler_duration %/% 60, input$howler_duration %% 60)
+  })
+
   return(
     list(
       playing = reactive(input$howler_playing),
